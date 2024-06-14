b10b4bdcdb415a97c11c3bf265f2fd58519387c3
Evgenii Zhemchugov <evgenii.zhemchugov@warwick.ac.uk>
2024-06-14 15:27:08 +0100

    Connection::Connection: initialize POD fields

--- caen.hpp
+++ caen.hpp
@@ -186,6 +186,8 @@ class Device {
       uint32_t                vme;
       std::string             ip;
 
+      Connection(): type(CAENComm_USB), arg(0), conet(0), vme(0) {};
+
       bool is_ethernet() const;
     };
 
