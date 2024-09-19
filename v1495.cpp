61cda3a0ce98ab8a4d6f15591d6a3e46d742e6c6
Evgenii Zhemchugov <evgenii.zhemchugov@warwick.ac.uk>
2024-09-19 19:23:06 +0100

    V1495: disable check for the board ID in the constructor
    After flashing with the user firmware, contents of OUI and ID registers
    changed and can no longer be used to identify the board.

--- v1495.cpp
+++ v1495.cpp
@@ -3,7 +3,9 @@
 namespace caen {
 
 V1495::V1495(const Connection& connection): Device(connection) {
-  if (oui() != OUI || id() != 1495) throw WrongDevice(connection, "V1495");
+  // It appears that V1495 OUI and ID registers may be overwritten with user
+  // firmware. So, we cannot check that we have connected to the proper board.
+  // if (oui() != OUI || id() != 1495) throw WrongDevice(connection, "V1495");
 };
 
 };
